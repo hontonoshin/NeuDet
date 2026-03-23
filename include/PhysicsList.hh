589a7a16525a2ed0e3205f3f4e097e738d05e0db
hontonoshin <146138410+hontonoshin@users.noreply.github.com>
2026-03-23 14:50:26 +0100

    Create PhysicsList.hh

---
+++ include/PhysicsList.hh
@@ -0,0 +1,17 @@
+//============================================================
+// PhysicsList.hh
+//============================================================
+#ifndef PhysicsList_h
+#define PhysicsList_h 1
+
+#include "G4VModularPhysicsList.hh"
+
+class PhysicsList : public G4VModularPhysicsList
+{
+public:
+    PhysicsList();
+    ~PhysicsList() override;
+    void SetCuts() override;
+};
+
+#endif
